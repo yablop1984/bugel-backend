0469542b67c8b1975527bdc6c67b14e10621e23d
yablop1984 <yablop@hotmail.com>
2025-05-31 21:28:34 -0500

    Update enviar.js
    cambios

--- api/enviar.js
+++ api/enviar.js
@@ -1,13 +1,21 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");  // <- Esta línea es clave
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();  // Respuesta al preflight
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).send('Solo se permiten solicitudes POST');
   }
 
   try {
-    const respuesta = await fetch("https://script.google.com/macros/s/AKfycbx5AxtL_nSmCZjs2bQXTA_bHj9mF9pN2OAjc5cFBL5qhJWg1fCPyJYo4ba5cmrvsXy9qA/exec", {
+    const respuesta = await fetch("https://script.google.com/macros/s/AKfycbx5AxtL_....", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(req.body)
+      body: JSON.stringify(req.body),
     });
 
     const texto = await respuesta.text();
