d1cee775db52c184e8ab10d75fb48dcc2cdb209b
yablop1984 <yablop@hotmail.com>
2025-05-31 20:20:38 -0500

    Create enviar.js

---
+++ api/enviar.js
@@ -0,0 +1,19 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).send('Solo se permiten solicitudes POST');
+  }
+
+  try {
+    const respuesta = await fetch("https://script.google.com/macros/s/AKfycbx5AxtL_nSmCZjs2bQXTA_bHj9mF9pN2OAjc5cFBL5qhJWg1fCPyJYo4ba5cmrvsXy9qA/exec", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(req.body)
+    });
+
+    const texto = await respuesta.text();
+    res.status(200).send(texto);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error al reenviar al Apps Script');
+  }
+}
